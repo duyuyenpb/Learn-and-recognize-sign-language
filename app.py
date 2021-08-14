d54d97513acbac2687f9d9a93932ab551d9b57f1
duyuyen <duyuyenphanbinh@gmail.com>
2021-08-14 23:07:16 +0700

    update

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route("/communicate", methods=['GET'])
-def communicate():
-    return render_template('communicate.html') 
-
 @app.route("/learn", methods=['GET'])
 def learn():
     return render_template('learn.html')
@@ -24,15 +20,6 @@ def flashcard():
 @app.route("/test", methods=['GET'])
 def test():
     return render_template('test.html')
-'''
-@app.route("/a", methods=['GET'])
-def a():
-    return render_template('a.html')
-@app.route("/communicate", methods=['POST'])
-def communicatePost():
-    if request.method == 'POST':
-        name = request.form['name']
-    return render_template('communicate.html', n = name)
-'''
+
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
