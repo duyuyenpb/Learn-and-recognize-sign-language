1a3432cbca06c2fd0665f0a605fa84e4c86ec405
vhchauu <vuhachau2412@gmail.com>
2021-08-14 13:41:58 +0700

    main: edited

---
+++ js/ques.js
@@ -0,0 +1,76 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Đây là chữ gì",
+    img: "./images/letterb.png",
+    answer: "Chữ B",
+    options: [
+      "Chữ A",
+      "Chữ D",
+      "Chữ C",
+      "Chữ B",
+    ]
+  },
+    {
+    numb: 2,
+    question: "Đây là chữ gì?",
+    img: "./images/lettera.png",
+    answer: "Chữ A",
+    options: [
+      "Chữ C",
+      "Chữ G",
+      "Chữ F",
+      "Chữ A",
+    ]
+  },
+    {
+    numb: 3,
+    question: "Đây là chữ gì?",
+    img: "./images/letterc.png",
+    answer: "Chữ C",
+    options: [
+      "Chữ A",
+      "Chữ C",
+      "Chữ H",
+      "Chữ D",
+    ]
+  },
+    {
+    numb: 4,
+    question: "Đây là chữ gì?",
+    img: "./images/letterd.png",
+    answer: "Chữ F",
+    options: [
+      "Chữ C",
+      "Chữ F",
+      "Chữ K",
+      "Chữ D"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Đây là chữ gì?",
+    img: "./images/letterb.png",
+    answer: "Chữ a",
+    options: [
+      "Chữ N",
+      "Chữ A",
+      "Chữ C",
+      "Chữ M",
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
