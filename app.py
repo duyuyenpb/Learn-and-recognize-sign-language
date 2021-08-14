1306433e0551104b9af12bb2886960d439e142d5
duyuyen <duyuyenphanbinh@gmail.com>
2021-08-14 15:21:48 +0700

    edit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
@@ -11,7 +11,7 @@ def about():
 
 @app.route("/communicate", methods=['GET'])
 def communicate():
-    return render_template('communicate.html')
+    return render_template('communicate.html') 
 
 @app.route("/learn", methods=['GET'])
 def learn():
@@ -24,6 +24,16 @@ def flashcard():
 @app.route("/test", methods=['GET'])
 def test():
     return render_template('test.html')
+
+@app.route("/a", methods=['GET'])
+def a():
+    return render_template('a.html')
+
+@app.route("/communicate", methods=['POST'])
+def communicatePost():
+    if request.method == 'POST':
+        name = request.form['name']
+    return render_template('communicate.html', n = name)
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
