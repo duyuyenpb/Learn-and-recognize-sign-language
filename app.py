bf2f1e88eccb556ec567b6382dc926400486cc3c
duyuyen <duyuyenphanbinh@gmail.com>
2021-08-14 20:57:07 +0700

    edit

--- app.py
+++ app.py
@@ -24,16 +24,15 @@ def flashcard():
 @app.route("/test", methods=['GET'])
 def test():
     return render_template('test.html')
-
+'''
 @app.route("/a", methods=['GET'])
 def a():
     return render_template('a.html')
-
 @app.route("/communicate", methods=['POST'])
 def communicatePost():
     if request.method == 'POST':
         name = request.form['name']
     return render_template('communicate.html', n = name)
-
+'''
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
