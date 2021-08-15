6a8b60e9c7aace863baf04315d8dba9549ced1bb
vhchauu <vuhachau2412@gmail.com>
2021-08-15 07:49:04 +0700

    main: last edit

--- static/js/ques.js
+++ static/js/ques.js
@@ -59,18 +59,5 @@ let questions = [
       "Chữ C",
       "Chữ M",
     ]
-  },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+  }
 ];
